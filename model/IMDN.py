b836b6abc20d66837583e5673fc2735fc9eb3f05
Your Name <you@example.com>
2022-02-21 14:13:30 +0800

    [cc]Add IMDN architecture and pretrained model

---
+++ model/IMDN.py
@@ -0,0 +1,152 @@
+import torch.nn as nn
+from collections import OrderedDict
+import torch
+
+class IMDN(nn.Module):
+    def __init__(self, in_nc=3, out_nc=3, nc=64, nb=8, upscale=4, act_mode='L', upsample_mode='pixelshuffle', negative_slope=0.05):
+        """
+        in_nc: channel number of input
+        out_nc: channel number of output
+        nc: channel number
+        nb: number of residual blocks
+        upscale: up-scale factor
+        act_mode: activation function
+        upsample_mode: 'upconv' | 'pixelshuffle' | 'convtranspose'
+        """
+        super(IMDN, self).__init__()
+        assert 'R' in act_mode or 'L' in act_mode, 'Examples of activation function: R, L, BR, BL, IR, IL'
+
+        m_head = conv(in_nc, nc, mode='C')
+        m_body = [IMDBlock(nc, nc, mode='C'+act_mode, negative_slope=negative_slope) for _ in range(nb)]
+        m_body.append(conv(nc, nc, mode='C'))
+
+
+        m_uper = upsample_pixelshuffle(nc, out_nc, mode=str(upscale))
+
+        self.model = sequential(m_head, ShortcutBlock(sequential(*m_body)), *m_uper)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+def upsample_pixelshuffle(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1, bias=True, mode='2R', negative_slope=0.2):
+    assert len(mode)<4 and mode[0] in ['2', '3', '4'], 'mode examples: 2, 2R, 2BR, 3, ..., 4BR.'
+    up1 = conv(in_channels, out_channels * (int(mode[0]) ** 2), kernel_size, stride, padding, bias, mode='C'+mode, negative_slope=negative_slope)
+    return up1
+
+def sequential(*args):
+    """Advanced nn.Sequential.
+
+    Args:
+        nn.Sequential, nn.Module
+
+    Returns:
+        nn.Sequential
+    """
+    if len(args) == 1:
+        if isinstance(args[0], OrderedDict):
+            raise NotImplementedError('sequential does not support OrderedDict input.')
+        return args[0]  # No sequential is needed.
+    modules = []
+    for module in args:
+        if isinstance(module, nn.Sequential):
+            for submodule in module.children():
+                modules.append(submodule)
+        elif isinstance(module, nn.Module):
+            modules.append(module)
+    return nn.Sequential(*modules)
+
+# --------------------------------------------
+# return nn.Sequantial of (Conv + BN + ReLU)
+# --------------------------------------------
+def conv(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True, mode='CBR', negative_slope=0.2):
+    L = []
+    for t in mode:
+        if t == 'C':
+            L.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias))
+        elif t == 'T':
+            L.append(nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias))
+        elif t == 'B':
+            L.append(nn.BatchNorm2d(out_channels, momentum=0.9, eps=1e-04, affine=True))
+        elif t == 'I':
+            L.append(nn.InstanceNorm2d(out_channels, affine=True))
+        elif t == 'R':
+            L.append(nn.ReLU(inplace=True))
+        elif t == 'r':
+            L.append(nn.ReLU(inplace=False))
+        elif t == 'L':
+            L.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=True))
+        elif t == 'l':
+            L.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=False))
+        elif t == '2':
+            L.append(nn.PixelShuffle(upscale_factor=2))
+        elif t == '3':
+            L.append(nn.PixelShuffle(upscale_factor=3))
+        elif t == '4':
+            L.append(nn.PixelShuffle(upscale_factor=4))
+        elif t == 'U':
+            L.append(nn.Upsample(scale_factor=2, mode='nearest'))
+        elif t == 'u':
+            L.append(nn.Upsample(scale_factor=3, mode='nearest'))
+        elif t == 'v':
+            L.append(nn.Upsample(scale_factor=4, mode='nearest'))
+        elif t == 'M':
+            L.append(nn.MaxPool2d(kernel_size=kernel_size, stride=stride, padding=0))
+        elif t == 'A':
+            L.append(nn.AvgPool2d(kernel_size=kernel_size, stride=stride, padding=0))
+        else:
+            raise NotImplementedError('Undefined type: '.format(t))
+    return sequential(*L)
+
+class IMDBlock(nn.Module):
+    """
+    @inproceedings{hui2019lightweight,
+      title={Lightweight Image Super-Resolution with Information Multi-distillation Network},
+      author={Hui, Zheng and Gao, Xinbo and Yang, Yunchu and Wang, Xiumei},
+      booktitle={Proceedings of the 27th ACM International Conference on Multimedia (ACM MM)},
+      pages={2024--2032},
+      year={2019}
+    }
+    @inproceedings{zhang2019aim,
+      title={AIM 2019 Challenge on Constrained Super-Resolution: Methods and Results},
+      author={Kai Zhang and Shuhang Gu and Radu Timofte and others},
+      booktitle={IEEE International Conference on Computer Vision Workshops},
+      year={2019}
+    }
+    """
+    def __init__(self, in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True, mode='CL', d_rate=0.25, negative_slope=0.05):
+        super(IMDBlock, self).__init__()
+        self.d_nc = int(in_channels * d_rate)
+        self.r_nc = int(in_channels - self.d_nc)
+
+        assert mode[0] == 'C', 'convolutional layer first'
+
+        self.conv1 = conv(in_channels, in_channels, kernel_size, stride, padding, bias, mode, negative_slope)
+        self.conv2 = conv(self.r_nc, in_channels, kernel_size, stride, padding, bias, mode, negative_slope)
+        self.conv3 = conv(self.r_nc, in_channels, kernel_size, stride, padding, bias, mode, negative_slope)
+        self.conv4 = conv(self.r_nc, self.d_nc, kernel_size, stride, padding, bias, mode[0], negative_slope)
+        self.conv1x1 = conv(self.d_nc*4, out_channels, kernel_size=1, stride=1, padding=0, bias=bias, mode=mode[0], negative_slope=negative_slope)
+
+    def forward(self, x):
+        d1, r1 = torch.split(self.conv1(x), (self.d_nc, self.r_nc), dim=1)
+        d2, r2 = torch.split(self.conv2(r1), (self.d_nc, self.r_nc), dim=1)
+        d3, r3 = torch.split(self.conv3(r2), (self.d_nc, self.r_nc), dim=1)
+        d4 = self.conv4(r3)
+        res = self.conv1x1(torch.cat((d1, d2, d3, d4), dim=1))
+        return x + res
+
+class ShortcutBlock(nn.Module):
+    def __init__(self, submodule):
+        super(ShortcutBlock, self).__init__()
+
+        self.sub = submodule
+
+    def forward(self, x):
+        output = x + self.sub(x)
+        return output
+
+    def __repr__(self):
+        tmpstr = 'Identity + \n|'
+        modstr = self.sub.__repr__().replace('\n', '\n|')
+        tmpstr = tmpstr + modstr
+        return tmpstr
